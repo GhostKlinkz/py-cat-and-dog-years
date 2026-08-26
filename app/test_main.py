2d2e4c4380c370655aed6829c2bfa727793553c6
GhostKlinkz <taralaroslav@gmail.com>
2026-08-26 19:29:16 +0300

    Update test_main.py

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,33 @@
 from app.main import get_human_age
 
-# write your code here
+
+def test_should_return_zero_for_zero_ages() -> None:
+    assert get_human_age(0, 0) == [0, 0]
+
+
+def test_should_return_zero_before_15_years() -> None:
+    assert get_human_age(14, 14) == [0, 0]
+
+
+def test_should_return_one_at_15_years() -> None:
+    assert get_human_age(15, 15) == [1, 1]
+
+
+def test_should_return_one_at_23_years() -> None:
+    assert get_human_age(23, 23) == [1, 1]
+
+
+def test_should_return_two_at_24_years() -> None:
+    assert get_human_age(24, 24) == [2, 2]
+
+
+def test_should_return_two_at_27_years() -> None:
+    assert get_human_age(27, 27) == [2, 2]
+
+
+def test_cat_and_dog_should_have_different_age_after_28_years() -> None:
+    assert get_human_age(28, 28) == [3, 2]
+
+
+def test_should_return_correct_age_for_100_years() -> None:
+    assert get_human_age(100, 100) == [21, 17]
